003947cc0f23db377c36be2b9a9bc64ab5dbdf2b
NovemberFoxtrot <november@fox.trot>
2013-08-02 01:53:31 -0700

    Hmmm

---
+++ dogberry.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"index/suffixarray"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+func main() {
+	sometext, err := ioutil.ReadFile(os.Args[1])
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	index := suffixarray.New(sometext)
+
+	offsets := index.Lookup([]byte("*"), -1)
+
+  for _, value := range offsets[0:10] {
+    log.Println(value, string(sometext[value]))
+  }
+}
